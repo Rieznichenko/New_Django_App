92724a30164d7a6fff47f7eee8f99c3750af55db
rtksoni <rtksoni00@gmail.com>
2024-06-04 15:19:36 +0530

    minor fix

--- gpt_discord/settings.py
+++ gpt_discord/settings.py
@@ -145,8 +145,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    BASE_DIR / "static",
 ]
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
