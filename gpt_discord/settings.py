cb59ddd18ff35547b112c9945704c397bd2d90bc
rtksoni <rtksoni00@gmail.com>
2024-06-04 15:30:11 +0530

    removed static file

--- gpt_discord/settings.py
+++ gpt_discord/settings.py
@@ -142,9 +142,7 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   BASE_DIR / "static",
-]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
