7dd8b77059a43d47b4fb80520add4b4fb1a3cfe8
git-freelance <git-freelance@yahoo.com>
2024-08-13 23:20:10 +0530

    fix sdb config

--- server_script.sh
+++ server_script.sh
@@ -26,6 +26,7 @@ sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
 sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
 sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+sudo -u postgres psql -c "ALTER DATABASE $DB_NAME OWNER TO $DB_USER;"
 
 # Set the virtual environment name
 VENV_NAME="venv"
