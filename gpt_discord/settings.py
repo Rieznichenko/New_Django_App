ccc22ddf321195c95d8b3fc1e385fd1554e9e554
git-freelance <git-freelance@yahoo.com>
2024-08-29 00:36:15 +0530

    dd new paage

--- gpt_discord/settings.py
+++ gpt_discord/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'gpt_discord.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'humanytekdb',
+        'USER': 'admin',
+        'PASSWORD': 'Greenisgood1!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
