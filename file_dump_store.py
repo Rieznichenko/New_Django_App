e1466f95f34fd16117cbe1ba02214e0a0c4ef464
git-freelance <134865509+git-freelance@users.noreply.github.com>
2024-09-12 13:54:06 +0530

    Add dump method

--- file_dump_store.py
+++ file_dump_store.py
@@ -5,21 +5,31 @@ from analytics.models import AnalyticOutput
 
 
 def dump_file_to_ftp(ftp_object_id, local_file_path):
-    obj = AnalyticOutput.objects.get(id = ftp_object_id)
     try:
-        # Connect to the FTP server
-        session = ftplib.FTP(obj.ftp_destination_server, obj.ftp_destination_user, obj.ftp_destination_password)
+        obj = AnalyticOutput.objects.get(id = ftp_object_id)
+        # Initialize SSH client
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         
-        # Change to the desired directory on the FTP server
-        session.cwd(obj.ftp_path)
+        # Connect to the SFTP server
+        ssh_client.connect(hostname=obj.ftp_destination_server, port=obj.ftp_destination_port, username=obj.ftp_destination_user, password=obj.ftp_destination_password)
         
-        # Open the local CSV file to be uploaded
-        with open(local_file_path, 'rb') as file:
-            # Store the file on the FTP server
-            session.storbinary(f'STOR {local_file_path.split("/")[-1]}', file)
+        # Open SFTP session
+        sftp_client = ssh_client.open_sftp()
         
-        print("File uploaded successfully")
+        # Change to the desired directory on the SFTP server
+        sftp_client.chdir(sftp_path)
+        
+        # Upload the file to the SFTP server
+        remote_file_name = local_file_path.split('/')[-1]
+        sftp_client.put(local_file_path, f'{sftp_path}/{remote_file_name}')
+        
+        print("File uploaded successfully to SFTP")
+        
+        # Close the SFTP connection
+        sftp_client.close()
+        ssh_client.close()
         
     except Exception as e:
-        print(f"Failed to dump file to ftp because {e}")
+        print(f"Failed to upload file to SFTP because {e}")
 
